694724b8e2aba5d6e48c907e316b53cd3e2894d3
Your Name <you@example.com>
2020-09-12 06:28:01 +0700

    menambahkan file

---
+++ index.js
@@ -0,0 +1,8 @@
+var listdesa = ['argodadi,sedayu','argomulyo,sedayu','argorejo,sedayu','argosari','bangunjiwo,kasihan','banguntapan,banguntapan','bantul,bantul','baturetno,banguntapan','bawuran,pleret','canden,jetis','caturharjo,pandak','cetan srigandi']
+document.write('<ol>');
+for (i = 0; i < listdesa.length; i++) {
+document.write('<li>');
+document.write(listdesa[i]);
+document.write('</li>')
+}
+document.write('</ol>');
